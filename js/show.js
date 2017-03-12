acbcbc29b71ec9559a46515ee2a909e35711ab4a
pmurcia <p.murcia.morilla@gmail.com>
2017-03-12 18:33:32 +0100

    Preliminary Template

---
+++ js/show.js
@@ -0,0 +1,9 @@
+$("button.type-option").click(function() {
+	$("button.type-option.btn-form-active").removeClass("btn-form-active");
+	$( this ).toggleClass( "btn-form-active" );
+});
+
+$("button.bet-option").click(function() {
+	$("button.bet-option.btn-form-active").removeClass("btn-form-active");
+	$( this ).toggleClass( "btn-form-active" );
+});
